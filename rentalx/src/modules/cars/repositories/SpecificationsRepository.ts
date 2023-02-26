56ec995539a97bdd6e99cfbca2d013693e02d2e2
joaopedroaquino <aqerhack@gmail.com>
2023-02-26 00:23:16 -0300

    Create specifications repository

--- rentalx/src/modules/cars/repositories/SpecificationsRepository.ts
+++ rentalx/src/modules/cars/repositories/SpecificationsRepository.ts
@@ -1,7 +1,17 @@
+import { Specification } from "../model/Specification";
 import { ICreateSpecificationDTO, ISpecificationsRepository } from "./ISpecificationsRepository";
 
 export class SpecificationsRepository implements ISpecificationsRepository {
+    private specifications: Specification[]
+    constructor(){this.specifications = [];};
     create({ description, name }: ICreateSpecificationDTO): void {
-    }
+        const specification = new Specification();
+        Object.assign(specification,{
+            name,
+            description,
+            created_at: new Date(),
+        });
+        this.specifications.push(specification);
+    };
 
-}
+};
