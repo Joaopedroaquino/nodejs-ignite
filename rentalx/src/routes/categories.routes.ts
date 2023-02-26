6a237fbd442a96004b25529f75a0570877580301
joaopedroaquino <aqerhack@gmail.com>
2023-02-25 23:08:02 -0300

    Create categoryCreateService

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -2,13 +2,17 @@ import { request, response, Router } from 'express';
 import { v4 as uuidV4 } from 'uuid';
 import { Category } from '../model/Category';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCatgoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
+    const { name, description } = request.body
+    const categoryService = new CreateCatgoryService(categoriesRepository);
+
+    categoryService.execute({name,description});
 
    
 
--- rentalx/src/services/CreateCategoryService.ts
+++ rentalx/src/services/CreateCategoryService.ts
@@ -6,14 +6,14 @@ interface IRequest {
 }
 export class CreateCatgoryService {
     constructor(private categoriesRepository: CategoriesRepository) { }
-    execute({ name, description }: IRequest) {
+    execute({ name, description }: IRequest): void {
         const categoryAlreadyExists = this.categoriesRepository.findByName(name);
         if (categoryAlreadyExists) {
             throw new Error("Category already exists")
 
         }
 
-        return this.categoriesRepository.create({ name, description })
+         this.categoriesRepository.create({ name, description })
     }
 
 }
