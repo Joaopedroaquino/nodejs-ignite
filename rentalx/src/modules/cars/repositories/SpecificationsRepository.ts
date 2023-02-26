b27f0879ae999c0b5a65da5dc5e819d5731a5153
joaopedroaquino <aqerhack@gmail.com>
2023-02-26 00:35:23 -0300

    create findByName method

--- rentalx/src/modules/cars/repositories/SpecificationsRepository.ts
+++ rentalx/src/modules/cars/repositories/SpecificationsRepository.ts
@@ -3,15 +3,21 @@ import { ICreateSpecificationDTO, ISpecificationsRepository } from "./ISpecifica
 
 export class SpecificationsRepository implements ISpecificationsRepository {
     private specifications: Specification[]
-    constructor(){this.specifications = [];};
+    constructor() { this.specifications = []; }
+    ;
     create({ description, name }: ICreateSpecificationDTO): void {
         const specification = new Specification();
-        Object.assign(specification,{
+        Object.assign(specification, {
             name,
             description,
             created_at: new Date(),
         });
         this.specifications.push(specification);
+    };
+
+    findByName(name: string): Specification {
+        const specification = this.specifications.find((specification) => specification.name === name);
+        return specification;
     };
 
 };
