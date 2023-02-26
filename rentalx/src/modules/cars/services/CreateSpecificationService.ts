c140a969cd906a2ff1610612e51a68b6d8c13bda
joaopedroaquino <aqerhack@gmail.com>
2023-02-26 00:26:26 -0300

    Create specifications use-case

--- rentalx/src/modules/cars/services/CreateSpecificationService.ts
+++ rentalx/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,5 +1,14 @@
-export class CreateSpecificationService {
-    execute(){
-        console.log("TODO")
-    }
+import { SpecificationsRepository } from "../repositories/SpecificationsRepository";
+
+export interface IRequest {
+    name: string;
+    description: string;
 }
+
+export class CreateSpecificationService {
+    constructor(private specificationRepository: SpecificationsRepository) { };
+    execute({ name, description }: IRequest): void {
+        this.specificationRepository.create({ name, description });
+
+    };
+};
