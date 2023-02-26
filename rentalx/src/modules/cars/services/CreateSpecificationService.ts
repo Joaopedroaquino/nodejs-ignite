207b29362d63d0c7a8696fc59136872285d75699
joaopedroaquino <aqerhack@gmail.com>
2023-02-26 00:38:02 -0300

    Throw if specification already exist

--- rentalx/src/modules/cars/services/CreateSpecificationService.ts
+++ rentalx/src/modules/cars/services/CreateSpecificationService.ts
@@ -8,6 +8,10 @@ export interface IRequest {
 export class CreateSpecificationService {
     constructor(private specificationRepository: SpecificationsRepository) { };
     execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists = this.specificationRepository.findByName(name);
+        if(specificationAlreadyExists){
+            throw new Error("Specification already exists!")
+        }
         this.specificationRepository.create({ name, description });
 
     };
