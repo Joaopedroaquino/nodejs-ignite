ac7a5a517a2e67df2cebf8160e3f8287a2f6a769
joaopedroaquino <aqerhack@gmail.com>
2023-03-01 17:37:11 -0300

    Remove unused imports

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,12 +1,6 @@
 import { request, response, Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
-import { Category } from '../modules/cars/model/Category';
-import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
-import { CreateCategoryUseCase } from '../modules/cars/useCases/createCategory/CreateCategoryUseCase';
 import { listCategoryController } from '../modules/cars/useCases/listCategories';
-import { ListCategoriesController } from '../modules/cars/useCases/listCategories/ListCategoriesController';
-import { ListCategoryUseCase } from '../modules/cars/useCases/listCategories/ListCategoryUseCase';
 import multer from 'multer';
 
 export const categoriesRoutes = Router();
