f49d9398d31d0008aad636a78148fb59c14b026d
joaopedroaquino <aqerhack@gmail.com>
2023-02-28 23:45:42 -0300

    ref: export routes

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,11 +1,11 @@
 import express, { request, response } from 'express';
+import { router } from './routes';
 import { categoriesRoutes } from './routes/categories.routes';
 import { specificationRoutes } from './routes/specification.routes';
 
 const app = express();
 app.use(express.json());
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationRoutes);
+app.use(router)
 
 
 
