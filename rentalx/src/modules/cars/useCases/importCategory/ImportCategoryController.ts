c8370177fe748f341d83c3dce0bb47c3dd7d0017
joaopedroaquino <aqerhack@gmail.com>
2023-03-04 22:14:04 -0300

    Create importCategoryController

---
+++ rentalx/src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+export class ImportCategoryController {
+    handle(request: Request, response: Response): Response{
+       const {file} = request
+       console.log(file) 
+      return response.send()  
+    }
+
+}
